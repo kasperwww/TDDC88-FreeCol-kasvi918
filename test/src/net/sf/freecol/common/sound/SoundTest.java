ae5ba44ef04128ae23266bf4aa8c6600c81fb0d5
Kasper Vinberg <kasvi918@student.liu.se>
2018-10-11 16:41:32 +0200

    mhhhH

--- test/src/net/sf/freecol/common/sound/SoundTest.java
+++ test/src/net/sf/freecol/common/sound/SoundTest.java
@@ -63,7 +63,7 @@ public class SoundTest extends FreeColTestCase {
         soundPlayer = null;
     }
 
-   // private void playSound(String id) {
+   private void playSound(String id) {
    //     File file = ResourceManager.getAudio(id);
     ///    assertNotNull("No sound resource: " + id, file);
     //    try {
